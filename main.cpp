e9fb1ec7dde534316815b185e79dcba8958230d6
Nic Costa <nic.costa@arm.com>
2017-06-15 15:33:49 -0500

    Turn on Power LED
    Set the power LED (D5,D6) to RED on startup.
    
    Signed-off-by: Nic Costa <nic.costa@arm.com>

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@
     By the ARM Reference Design (Red) Team
 */
 #include "mbed.h"
+#include "ChainableLED.h"
 
 // main() runs in its own thread in the OS
-int main() {
+int main()
+{
+    ChainableLED power_led(D5, D6, 1);
+
     printf("hello world\r\n");
+    power_led.setColorRGB(0, 1, 0, 0);
 }
 
