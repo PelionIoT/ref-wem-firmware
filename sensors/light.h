da4cf4f2e98ce660b6206a91f1a348d528c9da8e
Ryan Sherlock <ryan.m.sherlock@gmail.com>
2017-07-03 10:55:31 -0500

    [sensors] Adding sensors folders
    RED-162 #comment Adding new support for light sensors
    
    This patch adds the base sensor header file which initially
    defines the Sensor class and the AnalogSensor class.
    It also introduces the LightSensor and the more specific GL5528.
    
    Signed-off-by: Ryan Sherlock <ryan.m.sherlock@gmail.com>

---
+++ sensors/light.h
@@ -0,0 +1,62 @@
+#ifndef _FOTA_SENSOR_LIGHT_H
+#define _FOTA_SENSOR_LIGHT_H
+
+#include "mbed.h"
+
+#include "sensor.h"
+
+namespace fota {
+namespace sensor {
+namespace light {
+
+/** List of currently supported boards.
+ */
+enum BOARDS {
+    BOARD_GROVE_GL5528 = 0, /*!< Grove GL5528 Light Sensor */
+    BOARD_COUNT /*!< Number of current boards supported */
+};
+
+/** Base class for the light sensors.
+ */
+class Base
+{
+public:
+    /** Initialize the resources associated with the Light base class.
+     */
+    Base() : _lux(0.0f) { }
+
+    /** Teardown the Base class and clear up any resources associated with it.
+     */
+    ~Base() { }
+
+    /** Set the current flux value in luxes.
+     *
+     * @param flux The flux value measuere in luxes.
+     */
+    virtual void setFlux(float flux) {
+        _lux = flux;
+    };
+
+    /** Get the current flux value.
+     *
+     * @return Returns the current flux value in luxes.
+     */
+    virtual float getFlux(void) {
+        return _lux;
+    }
+
+protected:
+    float _lux;
+}; // end light::Base
+
+/** Template class for creating new boards.
+ */
+template <enum BOARDS B>
+class LightSensor
+{
+}; // end template<> LightSensor
+
+} // end namespace light
+} // end namespace sensor
+} // end namespace fota
+#endif
---
+++ sensors/sensor.h
@@ -0,0 +1,39 @@
+#ifndef _FOTA_SENSOR_H
+#define _FOTA_SENSOR_H
+
+namespace fota {
+namespace sensor {
+
+/** Base classes for sensors to inherit from.
+ * Defines a set of methods and members which should be implemented.
+ */
+class Sensor
+{
+public:
+    /** Get the most current value from the sensor.
+     */
+    virtual void update(void) = 0;
+}; // end Sensor
+
+/** AnalogSensor wrapper to the underlying mBedOS AnalogIn class.
+ */
+class AnalogSensor : Sensor
+{
+public:
+    /** Initialize the AnalogSensor on the PinName defined by pin.
+     *
+     * @param pin The pin to initialize the AnalogSensor on.
+     */
+    AnalogSensor(PinName pin) : _src(pin) { }
+
+    /** Teardown the AnalogSensor and clear up any resources associated with it.
+     */
+    ~AnalogSensor() { }
+protected:
+    AnalogIn _src;
+}; // end AnalogSensor
+
+} // end namespace sensor
+} // end namespace fota
+
+#endif
