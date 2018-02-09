5126ab93d1270aabcb381fff8ed81e64920ff931
Nic Costa <nic.costa@arm.com>
2018-02-08 20:31:55 -0600

    remove auto fcc-wipe code
    a previous commit removed the fcc-wipe build option but failed
    to actually delete the code, which created a bug where the fcc
    credentials were always deleted on every boot.
    
    Signed-off-by: Nic Costa <nic.costa@arm.com>

--- main.cpp
+++ main.cpp
@@ -1005,11 +1005,6 @@ static int init_fcc(void)
 {
     fcc_status_e ret;
 
-    ret = fcc_storage_delete();
-    if (ret != FCC_STATUS_SUCCESS) {
-        cmd.printf("ERROR: fcc delete failed: %d\n", ret);
-    }
-
     ret = fcc_init();
     if (ret != FCC_STATUS_SUCCESS) {
         cmd.printf("ERROR: fcc init failed: %d\n", ret);
